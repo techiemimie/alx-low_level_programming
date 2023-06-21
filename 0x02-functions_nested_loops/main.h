5ec37b36c0f93825d63b4810f661b3a4e1e8caed
techiemimie <ememprosper123@gmail.com>
2023-06-21 14:39:06 -0700

    3. islower

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,8 @@
 #ifndef main_h
 #define main_h
+#include <stdio.h>
 
-int _putchar(char);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -11,8 +12,8 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
+int add(int, int); 
 void print_to_98(int n);
-void print_alphabet(void);
+void print_times_table(int n);
 
 #endif
